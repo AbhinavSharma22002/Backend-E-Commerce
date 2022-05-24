2372a94c09c4de6ef895a37872741e681c91bbb5
Abhinav Sharma <jiofi22002@gmail.com>
2022-05-25 05:17:40 +0530

    hel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const connectToMongo = require("./db");
 connectToMongo();
-
+let server_port = process.env.YOUR_PORT || process.env.PORT || 500;
+let server_host = process.env.YOUR_HOST || '0.0.0.0';
 const express = require("express");
 const app = express();
-const port = 5000;
 const Cors = require("cors");
 app.use(Cors());
 app.use(express.json());
@@ -16,6 +16,10 @@ app.get("/", (req, res) => {
 app.use("/api/auth", require("./routes/auth.js"));
 app.use("/api/notes", require("./routes/notes.js"));
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+// app.listen(process.env.PORT || port, () => {
+//   console.log(`Listening at http://localhost:${port}`);
+// });
+
+app.listen(server_port, server_host, function() {
+  console.log('Listening on port %d', server_port);
 });
